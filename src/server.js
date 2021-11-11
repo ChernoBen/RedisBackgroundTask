75aa9365a83355997a2210646628e83f8a207f18
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-11-11 16:16:58 -0400

    setting up server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import UserController from './app/controllers/UserControllers'
+
+const app = express();
+app.use(express.json());
+
+app.post("/users",UserController.store);
+
+
+app.listen(3333,()=>{
+    console.log("Server running at port:3333");
+})
